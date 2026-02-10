00825baccd01923d683d1441fcd3524432393810
Daebok <138689677+YDaewon@users.noreply.github.com>
2026-02-11 01:53:32 +0900

    Update site configuration for blog details

--- site.config.ts
+++ site.config.ts
@@ -9,12 +9,12 @@ export default siteConfig({
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'Daebok blog',
+  domain: 'ydaewon-blog.com',
+  author: 'Daewon Yamg',
 
   // open graph metadata (optional)
-  description: 'Example Next.js Notion Starter Kit Site',
+  description: 'Test!',
 
   // social usernames (optional)
   twitter: 'transitive_bs',
